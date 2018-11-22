2475782561e2bbd966f3ed5a9413632e76d4e15c
liuwen <somjor@sina.com>
2018-11-22 17:46:34 +0800

    Update dfConst.h

--- dfConst.h
+++ dfConst.h
@@ -3,18 +3,30 @@
 #define COUNT 5
 #define RED 'x'
 #define BLUE '*'
-#define JUDGE 4
+#define JUDGE 9 
 //#define DEBUGM
 
+struct CountPerDirectionChess{
+	// array[0]-count;array[1]-up of chess ;array[2] - down of chess;
+	int Count_horizotal[3];
+	int Count_leftSlash[3];
+	int Count_vertical[3];
+	int Count_rightSlash[3];
+}cpdc={
+	{
+		{
+			1,0,0
+		},
+		{
+			1,0,0
+		},
+		{
+			1,0,0
+		},
+	}
+};
+ 
 
-struct cacheChessInfo{	
-	// Here cache location of chess; 
-	char upName[3];
-	char dName[3];
-	// count chess;		
-	int countUp;
-	int countD;	
-	// How many chess;
-	int countAmongUp;
-	int countAmongD;
-}cci_struct[4];
+
+
+
